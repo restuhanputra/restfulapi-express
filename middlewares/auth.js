9bac44cb41c2db4cb579af1a358b2e7575cd5119
restuhanputra <restu.hanputraa@gmail.com>
2022-10-11 13:00:19 +0700

    refactor: response code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,33 +1,25 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const JWT_SECRET = process.env.JWT_SECRET;
+const { response } = require('../utils/helper.utils');
 
 const auth = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   // if header auth is exist
   if (authHeader) {
-    console.log('bearer token:', authHeader);
-
     // split bearer token
     const token = authHeader.split(' ')[1];
 
     jwt.verify(token, JWT_SECRET, (err, user) => {
       if (err) {
-        return res.status(403).json({
-          status: 403,
-          message: 'Invalid token',
-        });
+        response(res, 401, null, null, err, 'Expired token');
       }
-
       req.user = user;
       next();
     });
   } else {
-    return res.status(401).json({
-      status: 401,
-      message: 'Invaled or expired token',
-    });
+    response(res, 401, null, null, null, 'Invalid token');
   }
 };
 
