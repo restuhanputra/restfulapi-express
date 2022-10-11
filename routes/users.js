e5c8ba886b5d24955562dc660e801346544f1f6c
restuhanputra <restu.hanputraa@gmail.com>
2022-10-11 12:58:17 +0700

    fix: middleware

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,9 @@ const userController = require('../controllers/user.controller');
 const authMiddleware = require('../middlewares/auth');
 
 /* GET users listing. */
-// router.get('/', authMiddleware.auth, userController.read);
 router.get('/', authMiddleware.auth, userController.read);
 router.get('/:id', authMiddleware.auth, userController.readById);
-router.post('/', authMiddleware.auth, userController.signup);
+router.post('/', userController.signup);
 router.patch('/:id', authMiddleware.auth, userController.update);
 router.delete('/:id', authMiddleware.auth, userController.destroy);
 router.post('/signin', userController.signin);
